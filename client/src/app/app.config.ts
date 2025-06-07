ffac6bd7435a499304e433eb26bc3e80b074bc59
Shaaz Anaam <anaamsz@mail.uc.edu>
2025-06-07 18:32:27 -0500

    End of applying the HTTPS

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+// remem that appConfig is a plain plain JavaScript object that matches the ApplciationConfig type 
+//it is not a function or a property . the object is exported so it can be imported and used elsewhere like in the main.ts file for the boot strapping the Angular application
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
