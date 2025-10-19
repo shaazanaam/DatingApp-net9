940f9f62e8dddc5b5e1021d56d421631022df610
Shaaz Anaam <anaamsz@mail.uc.edu>
2025-10-19 09:04:53 -0500

    Adding the main methods for why the primary constructor is being called by   the  custom object called as the AppDbContext which we are using

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -11,3 +11,15 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
       //Name of the table in the data base
       public DbSet<AppUserssss> Usersssses { get; set; }
 }
+
+
+// Property : public DbSet<AppUserssss> Userssss{get;set;}
+// this is the member of the AppDbContext class ( and not inherited from the Dbcontext)
+// It is of the type DbSet<AppUsersss> where AppUserss is another class (entity)
+// the property  name Usersssses is the string identifier within the AppDbContext object
+
+
+//Primary constructor: AppDbCOntext(DbContextOptions options) : DbContext(options)
+      // this is the constructor for the AppDbContext object
+      //it takes the DbContextoptions object as a parameter
+      //It calls the base class constructor (DbContext(options)) to initialize the inherited DbContext part of the object
