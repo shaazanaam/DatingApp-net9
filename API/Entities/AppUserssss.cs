865c57c23834149ac2bc8b1d4e54d3884ca6bb2e
Shaaz Anaam <anaamsz@mail.uc.edu>
2025-10-21 19:07:30 -0500

    Improved the namspace to contain overall reference and created the default contructor to have the GUID as the default value instead of the default string value for the user id

--- API/Entities/AppUserssss.cs
+++ API/Entities/AppUserssss.cs
@@ -1,5 +1,9 @@
 namespace API.Entities;
-
+// typically the n amespace represents the physical location of the place the file is located inside the 
+// these are refered to as the entity class becasuse they represent  a distint identifiable 
+// each instance of this entity represents the row in a table in the database
+// each property that this class has is going to be represnting a column  in the table 
+// 
 public class AppUserssss
 {
       public string Id { get; set; } = Guid.NewGuid().ToString();
